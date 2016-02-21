4e43e948f0661867450d8310e069bb3258506d52
juanshishido <juanshishido@cal.berkeley.edu>
2016-02-20 21:56:29 -0800

    add doctest

--- 7kyu/search_names.py
+++ 7kyu/search_names.py
@@ -1,3 +1,14 @@
 def search_names(logins):
+    """
+    Examples
+    --------
+    >>> search_names([['foo', 'foo@foo.com'], ['bar_', 'bar@bar.com']])
+    [['bar_', 'bar@bar.com']]
+    """
     assert isinstance(logins, list), 'Must be type list'
     return list(filter((lambda x: x[0][-1] == '_'), logins))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
